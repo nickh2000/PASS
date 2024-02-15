91e1ddd08993e18c179dfd5134cb0bbdc1217f47
unknown <nickhurley2000@gmail.com>
2024-02-15 18:35:20 -0500

    I2C support

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,9 +43,10 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
-I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
 
 SAI_HandleTypeDef hsai_BlockA1;
+DMA_HandleTypeDef hdma_sai1_a;
 
 UART_HandleTypeDef huart3;
 
@@ -61,9 +62,9 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_SAI1_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
-static void MX_USART3_UART_Init(void);
 static void MX_ADC1_Init(void);
-static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,16 +84,40 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 	avg_count ++;
 
 }
-//
-//void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai) {
-//
-//	  sprintf(MSG, "SAI sent");
-//	  HAL_UART_Transmit(&huart3, MSG, sizeof(MSG), 0xFFFF);
-//	  if (HAL_SAI_Transmit_IT(&hsai, (uint8_t*)&sai_fifo, 16) != HAL_OK){
-//	  	  Error_Handler();
-//	  }
-//
-//}
+
+void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai) {
+
+  for (int i = 15; i > 0; i --)
+	  sai_fifo[i] = sai_fifo[i - 1];
+  adc_avg = (adc_avg / avg_count);
+  sai_fifo[0] = (uint16_t)((short)((adc_get << 4) - (1 << 15)) >> 0) & (~0x3);
+//  sai_fifo[0] = 0x7000;//(adc_get << 4) - (1 << 15);
+  adc_avg = 0;
+  avg_count = 0;
+}
+
+#define ADAU_ADDR 0x04
+#define DAC_MUTE1 0x09
+#define PLL_CLK_CTRL0 0x00
+#define DAC_CTRL0 0x06
+
+
+void write_register(uint8_t reg, uint8_t* data) {
+	HAL_StatusTypeDef ret = HAL_I2C_Mem_Write(&hi2c2, ADAU_ADDR << 1, reg,
+            1, data, 1, HAL_MAX_DELAY);
+
+
+
+	char str_fail[100];
+	char str_success[100] = "Success occured!\r\n";
+	if (ret != HAL_OK) {
+		sprintf(str_fail, "Error is %d", HAL_I2C_GetError(&hi2c2));
+		HAL_UART_Transmit(&huart3, str_fail, strlen((char*)str_fail), HAL_MAX_DELAY);
+	} else {
+		HAL_UART_Transmit(&huart3, str_success, strlen((char*)str_success), HAL_MAX_DELAY);
+	}
+
+}
 
 /* USER CODE END 0 */
 
@@ -127,9 +152,9 @@ int main(void)
   MX_DMA_Init();
   MX_SAI1_Init();
   MX_USB_OTG_FS_PCD_Init();
-  MX_USART3_UART_Init();
   MX_ADC1_Init();
-  MX_I2C1_Init();
+  MX_I2C2_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -137,17 +162,34 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   HAL_ADC_Start_DMA(&hadc1, &adc_get, 1);
+  HAL_SAI_Transmit_DMA(&hsai_BlockA1, &sai_fifo, 16);
   sai_fifo[0] = 0x0F0FU;
+
+  HAL_Delay(1000);
+
+  char str[100] = "check\r\n";
+  uint8_t mute_data = 0;	//0 is normal operation, 1 is muted
+  uint8_t pll_clk_data = 1;
+  uint8_t dac_ctrl0 = 0b01100000;
+  uint8_t pll_clk_data1 = 0b01000000;
+  uint8_t dac_ctl1 = 0b10000;
+
+  HAL_UART_Transmit(&huart3, str, strlen((char*)str), HAL_MAX_DELAY);
+
+
+  write_register(PLL_CLK_CTRL0, &pll_clk_data);
+  write_register(DAC_MUTE1, &mute_data);
+  write_register(DAC_CTRL0, &dac_ctrl0);
+  write_register(0x07, &pll_clk_data1);
+  write_register(0x08, &dac_ctl1);
+
+
+
+  char str2[100] = "check2\r\n";
+  HAL_UART_Transmit(&huart3, str2, strlen((char*)str2), HAL_MAX_DELAY);
+
   while (1)
   {
-//	  for (int i = 15; i > 0; i --)
-//		  sai_fifo[i] = sai_fifo[i - 1];
-	adc_avg = (adc_avg / avg_count);
-	sai_fifo[0] = (uint16_t)((short)((adc_get << 4) - (1 << 15)) >> 1) & (~0x3);
-	HAL_SAI_Transmit(&hsai_BlockA1, &sai_fifo, 16, -1);
-
-	adc_avg = 0;
-	avg_count = 0;
 
     /* USER CODE END WHILE */
 
@@ -258,50 +300,50 @@ static void MX_ADC1_Init(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
+  * @brief I2C2 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_I2C1_Init(void)
+static void MX_I2C2_Init(void)
 {
 
-  /* USER CODE BEGIN I2C1_Init 0 */
+  /* USER CODE BEGIN I2C2_Init 0 */
 
-  /* USER CODE END I2C1_Init 0 */
+  /* USER CODE END I2C2_Init 0 */
 
-  /* USER CODE BEGIN I2C1_Init 1 */
+  /* USER CODE BEGIN I2C2_Init 1 */
 
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x00808CD2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x601015E9;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Analogue filter
   */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Digital filter
   */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
+  /* USER CODE BEGIN I2C2_Init 2 */
 
-  /* USER CODE END I2C1_Init 2 */
+  /* USER CODE END I2C2_Init 2 */
 
 }
 
@@ -337,7 +379,7 @@ static void MX_SAI1_Init(void)
   hsai_BlockA1.Init.CompandingMode = SAI_NOCOMPANDING;
   hsai_BlockA1.Init.TriState = SAI_OUTPUT_NOTRELEASED;
   hsai_BlockA1.FrameInit.FrameLength = 256;
-  hsai_BlockA1.FrameInit.ActiveFrameLength = 10;
+  hsai_BlockA1.FrameInit.ActiveFrameLength = 9;
   hsai_BlockA1.FrameInit.FSDefinition = SAI_FS_STARTFRAME;
   hsai_BlockA1.FrameInit.FSPolarity = SAI_FS_ACTIVE_HIGH;
   hsai_BlockA1.FrameInit.FSOffset = SAI_FS_FIRSTBIT;
@@ -437,6 +479,9 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream0_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+  /* DMA2_Stream1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
 
 }
 
@@ -454,6 +499,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
@@ -461,7 +507,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOG_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
@@ -474,12 +520,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
   HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD3_Pin LD2_Pin */
-  GPIO_InitStruct.Pin = LD3_Pin|LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  /*Configure GPIO pin : LD3_Pin */
+  GPIO_InitStruct.Pin = LD3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(LD3_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
   GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
@@ -511,11 +557,11 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  unsigned char error[25] = "Error occured!";
+  char str[100] = "Error handler!\r\n";
+  HAL_UART_Transmit(&huart3, str, strlen((char*)str), HAL_MAX_DELAY);
+
   while (1)
   {
-
-	 HAL_UART_Transmit(&huart3, error, sizeof(error), 0xFFFF);
   }
   /* USER CODE END Error_Handler_Debug */
 }
